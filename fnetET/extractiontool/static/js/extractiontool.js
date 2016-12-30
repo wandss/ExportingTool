6a5f10c9090b7e56bb35697e395166d52bb6e22f
wandss <wandss@gmail.com>
2016-12-29 22:32:47 -0200

    created validation for field with the server name

--- fnetET/extractiontool/static/js/extractiontool.js
+++ fnetET/extractiontool/static/js/extractiontool.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 
-	$('#available_servers').change(function(){		
-		var server = document.getElementById('available_servers').value;				
+	$('#available_servers').change(function(){
+		var server = document.getElementById('available_servers').value;
 		$("#myform").submit();
 	});
-	
+
 	$("#available_classes").change(function(){
 		var chosen_class = document.getElementById('available_classes').value;
 		$("#myform").submit();
-	});	
+	});
 	$("#class_properties").click(function(){
 		var property = $(this).val();
 		$("#chosen_props").val(property);
@@ -17,12 +17,12 @@ $(document).ready(function(){
 	$('#left_menu').mouseenter(function(){
 		$('#left-well').css('box-shadow', '-7px -1px 33px 2px #444');
 		$('#center-well').css('box-shadow', '0px 0px 0px 0px #444');
-		
-		
+
+
 	});
 	$('#left_menu').mouseleave(function(){
 		$('#left-well').css('box-shadow', '0px 0px 0px 0px #444');
-		$('#center-well').css('box-shadow','-7px -1px 33px 2px #444');  
+		$('#center-well').css('box-shadow','-7px -1px 33px 2px #444');
 	});
 
 	$('#right-menu').mouseenter(function(){
@@ -31,17 +31,17 @@ $(document).ready(function(){
 	});
 	$('#right-menu').mouseleave(function(){
 		$('#rigth-well').css('box-shadow', '0px 0px 0px 0px #444');
-		$('#center-well').css('box-shadow','-7px -1px 33px 2px #444');  
+		$('#center-well').css('box-shadow','-7px -1px 33px 2px #444');
 	});
 	$("#username, #passwd").change(function(){
 		username = document.getElementById('username').value;
 		passwd = document.getElementById('passwd').value;
-		
-		if (username !== '' & passwd !== '' ){			
-			$("#available_servers").removeAttr('disabled');			
+
+		if (username !== '' & passwd !== '' ){
+			$("#available_servers").removeAttr('disabled');
 		}
 		else{
-			$("#available_servers").attr('disabled', 'true');			
+			$("#available_servers").attr('disabled', 'true');
 		}
 	});
 	$("#server, #delete_server").mouseenter(function(){
@@ -52,23 +52,34 @@ $(document).ready(function(){
 	$("#server, #delete_server").mouseleave(function(){
 		$(this).css('background-color', '');
 		$(this).css('text-shadow', '');
-	});	
+	});
 
-	$(".list-group-item, btn ").click(function(){		
+	$(".list-group-item, btn ").click(function(){
 		var server_address = $(this).attr('address');
-		var server_name =  $(this).attr('serverName');	
-		var server_pk = $(this).attr('serverPk');	 			
-		$("#server_name").val(server_name);		
+		var server_name =  $(this).attr('serverName');
+		var server_pk = $(this).attr('serverPk');
+		$("#server_name").val(server_name);
 		$("#server_address").val(server_address);
 		$('#edit').val(server_pk);
 	});
+	$("#save_set").click(function(){
+		var server_name = $("#server_name").val();
+		if (server_name.length > 18){
+			$(".my_h6").prepend('<div class="warning_message" style="color: red"><em>This field is limited to 18 Characters!!!</em></div>');
+			return false;
+		}
+		else {
+			$("#myform").submit();
+		}
+
+	});
 	username = document.getElementById('username').value;
-	passwd = document.getElementById('passwd').value;		
-	if (username !== '' & passwd !== '' ){			
-		$("#available_servers").removeAttr('disabled');			
+	passwd = document.getElementById('passwd').value;
+	if (username !== '' & passwd !== '' ){
+		$("#available_servers").removeAttr('disabled');
 	}
 	else{
-		$("#available_servers").attr('disabled', 'true');			
+		$("#available_servers").attr('disabled', 'true');
 	}
 
 })
