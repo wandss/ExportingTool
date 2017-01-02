2df1123b84f21c9c898038249b62b22380cc2227
wandss <wandss@gmail.com>
2017-01-02 20:02:31 -0200

    Added server info functionality

--- fnetET/extractiontool/views.py
+++ fnetET/extractiontool/views.py
@@ -8,7 +8,7 @@ from getContentWithIDAnyClass import GetDocuments
 from extractiontool.models import *
 
 def index(request):
-	context = {}	
+	context = {}
 	if request.method == 'POST':
 
 		if request.POST.get('server_name'):
@@ -16,17 +16,17 @@ def index(request):
 			server_name = request.POST.get('server_name')
 			server_address = request.POST.get('server_address')
 			edit = request.POST.get('edit')
-			if edit:				
+			if edit:
 				CmisServer.objects.filter(pk = edit).update(
 					server_name=server_name,
 					server_address=server_address)
 				return redirect('/')
-			CmisServer(server_name=server_name, 
+			CmisServer(server_name=server_name,
 				server_address=server_address).save()
 			return redirect('/')
 
 		if not request.POST.get('chosen_props'):
-			"""Getting the Available Classes from a given server"""	
+			"""Getting the Available Classes from a given server"""
 			if not request.session.keys():
 				user = request.POST.get('username')
 				passwd = request.POST.get('passwd')
@@ -35,41 +35,41 @@ def index(request):
 			else:
 				user = request.session['user']
 				passwd = request.session['passwd']
-		
-			chosen_server = request.POST.get('available_servers')			
-			server = CmisServer.objects.filter(server_name = chosen_server)	
-			context['servers'] = server
-			context['chosen_server'] = True					
-			fnet = Fnet(server[0].server_address, user, passwd)
 
+			chosen_server = request.POST.get('available_servers')
+			server = CmisServer.objects.filter(server_name = chosen_server)
+			context['servers'] = server
+			context['chosen_server'] = True
+			fnet = Fnet(server[0].server_address, user, passwd)
+			context['server_info'] = fnet.rep.info
 			if type(fnet.rep) == type(str()):
 				context['errors'] = {'Error' : fnet.rep}
 				return render(request, "index.html", context)
 
-			if server and not request.POST.has_key('selected_class'):				
-				available_classes = fnet.getFnetClasses()								
+			if server and not request.POST.has_key('selected_class'):
+				available_classes = fnet.getFnetClasses()
 				context['available_classes'] = available_classes
 
-				return render(request, "index.html", context)	
+				return render(request, "index.html", context)
 
-			myclass = request.POST.get('selected_class')					
+			myclass = request.POST.get('selected_class')
 			fnet_class = fnet.getClass(myclass)
 			context['available_classes'] = [fnet_class]
 			properties = fnet.removeSystemProperties(fnet_class.properties)
 			context['properties'] = properties
 			context['available_classes']
-			return render(request, "index.html", context)	
-		
+			return render(request, "index.html", context)
+
 		user = request.session['user']
-		passwd = request.session['passwd']	
-		chosen_server = request.POST.get('available_servers')			
+		passwd = request.session['passwd']
+		chosen_server = request.POST.get('available_servers')
 		server = CmisServer.objects.filter(server_name = chosen_server)
 		fnet = Fnet(server[0].server_address, user, passwd)
 		chosen_properties = request.POST.get('chosen_props').split(',')
 		doc_class = request.POST.get('selected_class')
-		fnet_ids = [ids.strip('\r') for ids in request.POST.get('fnet_ids').split('\n')]		
+		fnet_ids = [ids.strip('\r') for ids in request.POST.get('fnet_ids').split('\n')]
 		saveDocs = GetDocuments(chosen_properties, fnet.rep, fnet_ids, doc_class)
-				
+
 		if saveDocs.errors:
 			context['errors'] = saveDocs.errors
 
@@ -81,15 +81,15 @@ def index(request):
 			context['saved_servers'] = servers
 		return render(request, "index.html", context)
 
-	context['servers'] = servers	
+	context['servers'] = servers
 	return render(request, "index.html", context)
 
 
-def remove(request):	
-	CmisServer.objects.filter(pk=request.GET.keys()[0]).delete()	
+def remove(request):
+	CmisServer.objects.filter(pk=request.GET.keys()[0]).delete()
 	return redirect('/')
 
-def appinfo(request):	
+def appinfo(request):
 	page = request.GET.keys()[0]
 	print page
 	context = {page : True,
